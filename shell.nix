e27677f6e182d99099d48eacd56f00e10ca2ea55
Jamie Brandon <jamie@scattered-thoughts.net>
2021-05-15 16:14:32 -0700

    Bump to zig master

--- shell.nix
+++ shell.nix
@@ -11,11 +11,8 @@ let
   zig = pkgs.stdenv.mkDerivation {
     name = "zig";
     src = fetchTarball (if (pkgs.system == "x86_64-linux") then {
-        url = "https://ziglang.org/download/0.7.1/zig-linux-x86_64-0.7.1.tar.xz";
-        sha256 = "1jpp46y9989kkzavh73yyd4ch50sccqgcn4xzcflm8g96l3azl40";
-    } else if (pkgs.system == "aarch64-linux") then {
-        url = "https://ziglang.org/download/0.7.1/zig-linux-aarch64-0.7.1.tar.xz";
-        sha256 = "02fvph5hvn5mrr847z8zhs35kafhw5pik6jfkx3rimjr65pqpd9v";
+        url = "https://ziglang.org/builds/zig-linux-x86_64-0.8.0-dev.2271+7a4b53fde.tar.xz";
+        sha256 = "1a0wj7sdca3qlif6i1ircyx7wsmsdfaw19kyzjbqfazga2jh9kmy";
     } else throw ("Unknown system " ++ pkgs.system));
     dontConfigure = true;
     dontBuild = true;
