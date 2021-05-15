38f17fdb3638a10b4041e5e007c88490874e6c91
Jamie Brandon <jamie@scattered-thoughts.net>
2021-05-15 14:19:53 -0700

    Wrapped allocator

--- main.zig
+++ main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
 const Expr = union(enum) {
     Constant: u64,
@@ -12,7 +13,7 @@ const Expr = union(enum) {
     },
 };
 
-pub fn eval(allocator: *std.mem.Allocator, expr: Expr) error{OutOfMemory}![]const []const u64 {
+pub fn eval(allocator: *Allocator, expr: Expr) error{OutOfMemory}![]const []const u64 {
     switch (expr) {
         .Constant => |number| {
             const row = try allocator.alloc(u64, 1);
@@ -43,8 +44,35 @@ pub fn eval(allocator: *std.mem.Allocator, expr: Expr) error{OutOfMemory}![]cons
     }
 }
 
+const BoundedAllocator = struct {
+    parent: *Allocator,
+    allocator: Allocator,
+
+    fn init(parent: *Allocator) BoundedAllocator {
+        return .{
+            .parent = parent,
+            .allocator = Allocator{
+                .allocFn = alloc,
+                .resizeFn = resize,
+            },
+        };
+    }
+
+    fn alloc(allocator: *Allocator, n: usize, ptr_align: u29, len_align: u29, ra: usize) ![]u8 {
+        const self = @fieldParentPtr(BoundedAllocator, "allocator", allocator);
+        return self.parent.allocFn(self.parent, n, ptr_align, len_align, ra);
+    }
+
+    fn resize(allocator: *Allocator, buf: []u8, buf_align: u29, new_len: usize, len_align: u29, ret_addr: usize) Allocator.Error!usize {
+        const self = @fieldParentPtr(BoundedAllocator, "allocator", allocator);
+        return self.parent.resizeFn(self.parent, buf, buf_align, new_len, len_align, ret_addr);
+    }
+};
+
 pub fn main() !void {
-    const allocator = std.heap.c_allocator;
+    var parent_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    var bounded_allocator = BoundedAllocator.init(&parent_allocator.allocator);
+    const allocator = &bounded_allocator.allocator;
     const expr_0 = Expr{ .Constant = 0 };
     const expr_1 = Expr{ .Constant = 1 };
     const expr_01 = Expr{ .Union = .{ .left = &expr_0, .right = &expr_1 } };
